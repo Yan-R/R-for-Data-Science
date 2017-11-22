52499e17547f26b6f1f689ec43aad3165709da85
Richard Yan <richardyan97@gmail.com>
2017-11-22 11:11:24 +1100

    Completed

--- 11_data_import.R
+++ 11_data_import.R
@@ -87,4 +87,104 @@ guess_encoding(charToRaw(x2))
 
 # 11.3.3 Factors ----------------------------------------------------------
 
+fruits <- c("banana", "apple", "mango")
+parse_factor(c("banana", "apple", "apple", "mango", "pineapple"), levels = fruits)
+parse_factor(c("banana", "apple", "apple", "mango"), levels = fruits)
 
+# But if you have many problematic entries, it’s often easier to leave as character 
+# vectors and then use the tools you’ll learn about in strings and factors to clean 
+# them up.
+
+
+# 11.3.4 Dates, date-times, and times -------------------------------------
+
+parse_datetime(c("2017-11-21T224707", "2017-11-21T224737"))
+
+parse_date(c("2014-11-18", "2017/11/21"))
+
+parse_time("01:18 pm")
+
+parse_time("22:50:08")
+
+# If the data that we are importing / attempting to parse has an unconventional
+# format for date and/or time:
+
+parse_date("21/11/2017", format = "%d/%m/%Y")
+
+parse_date("21/11/17", format = "%d/%m/%y")
+
+parse_date("21st Nov, 2017", format = "%d%.%.%.%b%.%.%Y")
+
+parse_date("13th Apr, 2018", format = "%d%.%.%.%b%.%.%Y")
+
+parse_time("1456", format = "%H%M")
+
+
+# 11.3.5 Exercises --------------------------------------------------------
+
+locale = locale(decimal_mark = ".", grouping_mark = ",", encoding = "UTF-8")
+
+parse_number(c("200,123.67", "214,623.62"), locale = locale(decimal_mark = ".",
+                                                            grouping_mark = "."))
+
+AUS_LOCALE <- locale(decimal_mark = ".", grouping_mark = ",", encoding = "UTF-8")
+
+d1 <- "January 1, 2010"
+d2 <- "2015-Mar-07"
+d3 <- "06-Jun-2017"
+d4 <- c("August 19 (2015)", "July 1 (2015)")
+d5 <- "12/30/14" # Dec 30, 2014
+t1 <- "1705"
+t2 <- "11:15:10.12 PM"
+
+parse_date(d1, format = "%B%.%d%.%.%Y")
+parse_date(d1, format = "%B %d, %Y")
+parse_date(d2, format = "%Y-%b-%d")
+parse_date(d2, format = "%Y%.%b%.%d")
+parse_date(d3, format = "%d-%b-%Y")
+parse_date(d4, format = "%B %d %.%Y%.")
+parse_date(d5, format = "%m/%d/%y")
+parse_time(t1, format = "%H%M")
+parse_time(t2, format = "%I:%M:%OS %p")
+
+
+# 11.4 Parsing a file -----------------------------------------------------
+
+guess_parser(c("1", "2", "5", "3"))
+guess_parser(c("2017/11/22", "2017-11-25"))
+guess_parser(c("T", "F", "T", "T"))
+guess_parser(c("12,412,736", "239,231"))
+
+str(parse_guess("2010-10-10"))
+
+challenge <- read_csv(readr_example("challenge.csv"))
+problems(challenge)
+# Lots of problems with column x. There are trailing characters where it expects none
+# because readr has parsed assuming integers. Fixing this:
+challenge <- read_csv(
+  readr_example("challenge.csv"),
+  col_types = cols(
+    x = col_double(),
+    y = col_character()
+  )
+)
+tail(challenge)
+challenge <- read_csv(
+  readr_example("challenge.csv"),
+  col_types = cols(
+    x = col_double(),
+    y = col_date()
+  )
+)
+tail(challenge)
+
+# Always try to supply col_types = cols() with the read_csv() function!!! This keeps
+# it consistent and easy to spot mistakes.
+
+challenge2 <- read_csv(readr_example("challenge.csv"), guess_max = 1001)
+problems(challenge2) # i.e. NONE!
+
+challenge3 <- read_csv(readr_example("challenge.csv"),
+                       col_types = cols(.default = col_character()))
+challenge3
+type_convert(challenge3)
