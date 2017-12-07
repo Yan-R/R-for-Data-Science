cf1a29d40b1dd2c2504d3514a6e8028f1191f3c2
Richard Yan <richardyan97@gmail.com>
2017-12-07 22:18:09 +1100

    Completed

--- 16_dates_and_times.R
+++ 16_dates_and_times.R
@@ -57,3 +57,27 @@ flights_dt %>%
   filter(dep_time < ymd(20130102)) %>%
   ggplot() +
   geom_freqpoly(mapping = aes(x = dep_time), binwidth = 600)
+
+
+# 16.2.3 From other types -------------------------------------------------
+
+today()
+now()
+
+as_datetime(today())
+as_date(now())
+
+
+ymd("2010-10-01", "bananas")
+
+d1 <- "January 1, 2010"
+d2 <- "2015-Mar-07"
+d3 <- "06-Jun-2017"
+d4 <- c("August 19 (2015)", "July 1 (2015)")
+d5 <- "12/30/14" # Dec 30, 2014
+
+mdy(d1)
+ymd(d2)
+dmy(d3)
+mdy(d4)
+mdy(d5)
